5b44d9d0abc88bcb0c4d1c7633844b175356c0c0
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2023-01-14 22:27:30 -0400

    added Engineer Profile Test

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,25 +1,25 @@
-const Manager = require("../lib/Manager");
+const Engineer = require("../lib/Engineer");
 
 describe('Engineer Profile', () => {
   
   it('After user filled the data, export HTML codes', () => {
-    const answers = { name: 'Elvan', id: '1', email: 'elvan@gmail.com', office: '234' };
+    const answers = { name: 'Yel', id: '2', email: 'yza@gmail.com', github: 'yelzaw' };
     const resultHTML =
-    `<div class="card">
-        <div class="card-header">
-                <h2 class="card-title">Elvan</h2>
-                <h3 class="card-title">Manager</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: 1</li>
-                <li class="list-group-item">Email:<a href="mailto:elvan@gmail.com:">elvan@gmail.com</a></li>
-                <li class="list-group-item">Office number:234</li>
-            </ul>
-        </div>
-        </div>
-        `
-      const resultFinal = new Manager().generateManager(answers);
+`<div class="card">
+<div class="card-header">
+<h2 class="card-title">Yel</h2>
+<h3 class="card-title">Engineer</h3>
+</div>
+<div class="card-body">
+<ul class="list-group">
+<li class="list-group-item">ID: 2</li>
+<li class="list-group-item">Email:<a href="mailto:yza@gmail.com:">yza@gmail.com</a></li>
+<li class="list-group-item">Github:<a href="https://github.com/yelzaw">yelzaw</a></li>
+</ul>
+</div>
+</div>
+`
+      const resultFinal = new Engineer().generateEngineer(answers);
 
       expect(resultFinal).toEqual(resultHTML);
     });
---
+++ tests/Intern.test.js
@@ -0,0 +1,5 @@
+const Intern = require("../lib/Intern")
+
+it('After user filled the data, export HTML codes', () => {
+    const answers = { name: 'Zaki', id: '3', school: 'Forest Hill' };
+    const resultHTML =
