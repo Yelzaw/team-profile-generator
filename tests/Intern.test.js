e79db592f804762b36aa75edfe06244dc4b4701c
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2023-01-14 22:31:29 -0400

    added Intern Profile Test

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,5 +1,25 @@
 const Intern = require("../lib/Intern")
 
-it('After user filled the data, export HTML codes', () => {
+describe('Intern Profile', () => {
+
+    it('After user filled the data, export HTML codes', () => {
     const answers = { name: 'Zaki', id: '3', school: 'Forest Hill' };
     const resultHTML =
+`<div class="card">
+<div class="card-header">
+<h2 class="card-title">Zaki</h2>
+<h3 class="card-title">Intern</h3>
+</div>
+<div class="card-body">
+<ul class="list-group">
+<li class="list-group-item">ID: 3</li>
+<li class="list-group-item">School:Forest Hill</li>
+</ul>
+</div>
+</div>
+`
+    const resultFinal = new Intern().generateIntern(answers);
+
+      expect(resultFinal).toEqual(resultHTML);
+    });
+ });
