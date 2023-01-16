e54686cf9911883b2263446fca5b1861ce8ec964
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2023-01-15 22:52:41 -0400

    recall last saved

--- index.js
+++ index.js
@@ -6,8 +6,3 @@ const employee = new Employee ();
 // Start application
 employee.creatTeam();
 
-// .then((member)=>{
-//    fs.writeFile(`./sample-index.html`,member,(err)=>
-//    err ? console.log(err) : console.log('Generating Team Profile ...')
-//    );
-//    })
